f1d06a6aafcf3c515cdb08eefbad182f9d1c5a89
sunkaixuan <610262374@qq.com>
2016-10-30 06:22:36 +0800

    ok

---
+++ NewTest/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    public class Test
+    {
+        
+        public Int64 Id {get;set;}
+
+        public DateTime Date {get;set;}
+
+    }
+}
